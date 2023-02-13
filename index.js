b5d1aa5f423b624fe4e295a05418c0f7913c48cb
evilNotready <bogatov.pasha78@gmail.com>
2023-02-13 05:47:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let popup = document.getElementById("popup")
+function openPopupWrl(){
+    popup.classList.add("open-popupWrl");
+}
+function closePopupWrl(){
+    popup.classList.remove("open-popupWrl");
+}
+
+
+let popupT = document.getElementById("popup")
+function openPopupT(){
+    popupT.classList.add("open-popupT");
+}
+function closePopupT(){
+    popupT.classList.remove("open-popupT");
+}
+
+
