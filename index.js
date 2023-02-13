796ecfbbd920f58797ed9b6f0bc4b4db85abaebe
evilNotready <bogatov.pasha78@gmail.com>
2023-02-14 03:51:39 +0500

    update

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 let popup = document.getElementById("popup")
-function openPopupWrl(){
+function openPopupWrl() {
     popup.classList.add("open-popupWrl");
 }
-function closePopupWrl(){
+function closePopupWrl() {
     popup.classList.remove("open-popupWrl");
 }
-
-
-let popupT = document.getElementById("popup")
-function openPopupT(){
+let popupT = document.getElementById("popupT")
+function openPopupT() {
     popupT.classList.add("open-popupT");
 }
-function closePopupT(){
+function closePopupT() {
     popupT.classList.remove("open-popupT");
 }
+let popupH = document.getElementById("popupH")
+function openPopupH() {
+    popupH.classList.add("open-popupH");
+}
+function closePopupH() {
+    popupH.classList.remove("open-popupH");
+}
+let popupX = document.getElementById("popupX")
+function openPopupX() {
+    popupX.classList.add("open-popupX");
+}
+function closePopupX() {
+    popupX.classList.remove("open-popupX");
+}
+let popupP = document.getElementById("popupP")
+function openPopupP() {
+    popupP.classList.add("open-popupP");
+}
+function closePopupP() {
+    popupP.classList.remove("open-popupP");
+}
+
 
 
