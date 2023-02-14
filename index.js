08ecff1d831ff47b1463dd50fbd88067e3624263
evilNotready <bogatov.pasha78@gmail.com>
2023-02-14 07:10:33 +0500

    d&d

--- index.js
+++ index.js
@@ -33,6 +33,59 @@ function openPopupP() {
 function closePopupP() {
     popupP.classList.remove("open-popupP");
 }
-
-
-
+let popupS = document.getElementById("popupS")
+function openPopupS() {
+    popupS.classList.add("open-popupS");
+}
+function closePopupS() {
+    popupS.classList.remove("open-popupS");
+}
+let popupI = document.getElementById("popupI")
+function openPopupI() {
+    popupI.classList.add("open-popupI");
+}
+function closePopupI() {
+    popupI.classList.remove("open-popupI");
+}
+let popupHr = document.getElementById("popupHr")
+function openPopupHr() {
+    popupHr.classList.add("open-popupHr");
+}
+function closePopupHr() {
+    popupHr.classList.remove("open-popupHr");
+}
+let popupA = document.getElementById("popupA")
+function openPopupA() {
+    popupA.classList.add("open-popupA");
+}
+function closePopupA() {
+    popupA.classList.remove("open-popupA");
+}
+let popupSw = document.getElementById("popupSw")
+function openPopupSw() {
+    popupSw.classList.add("open-popupSw");
+}
+function closePopupSw() {
+    popupSw.classList.remove("open-popupSw");
+}
+let popupSt = document.getElementById("popupSt")
+function openPopupSt() {
+    popupSt.classList.add("open-popupSt");
+}
+function closePopupSt() {
+    popupSt.classList.remove("open-popupSt");
+}
+let popupSh = document.getElementById("popupSh")
+function openPopupSh() {
+    popupSh.classList.add("open-popupSh");
+}
+function closePopupSh() {
+    popupSh.classList.remove("open-popupSh");
+}
+let popupSx = document.getElementById("popupSx")
+function openPopupSx() {
+    popupSx.classList.add("open-popupSx");
+}
+function closePopupSx() {
+    popupSx.classList.remove("open-popupSx");
+}
